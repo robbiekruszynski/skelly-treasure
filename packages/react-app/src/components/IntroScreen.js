cba61f2971bc0167f4a7a15bdfa43d395c595532
robbie <robbiekruszynski@gmail.com>
2020-06-08 16:15:46 -0700

    make bones for new component to render as intro screen

---
+++ packages/react-app/src/components/IntroScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
+export const IntroScreen = () => {
+  return (
+    <div>
+      <p>Intro Screen Render</p>
+    </div>
+  );
+};

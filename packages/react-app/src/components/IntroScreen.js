394d74bf64e8bb05a990b39b43c84be478f4d4b7
robbie <robbiekruszynski@gmail.com>
2020-06-12 08:53:10 -0700

    render basic navbar on /home

--- packages/react-app/src/components/IntroScreen.js
+++ packages/react-app/src/components/IntroScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ethers } from "ethers";
 import "./IntroScreen.scss";
+import NavBar from "./NavBar";
 import {
   BrowserRouter as Router,
   Link,
@@ -12,25 +13,28 @@ export const IntroScreen = ({ history }) => {
   let ethereum = window.ethereum;
 
   return (
-    <div className="IntroBody">
-      <p>Skelly Treasure</p>
-      <Link>
-        <button
-          onClick={() => {
-            history.push("/game");
-          }}
-        >
-          click for game
-        </button>
-        <button
-          variant="contained"
-          color="primary"
-          // class="enableEthereumButton"
-          onClick={() => ethereum.enable()}
-        >
-          Wallet Check
-        </button>
-      </Link>
+    <div className="MainContainer">
+      <NavBar />
+      <div className="IntroBody">
+        <p>Skelly Treasure</p>
+        <Link>
+          <button
+            onClick={() => {
+              history.push("/game");
+            }}
+          >
+            click for game
+          </button>
+          <button
+            variant="contained"
+            color="primary"
+            // class="enableEthereumButton"
+            onClick={() => ethereum.enable()}
+          >
+            Wallet Check
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
---
+++ packages/react-app/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./NavBar.scss";
+const NavBar = () => {
+  return (
+    <div className="NavBar">
+      <p>Navbar</p>
+    </div>
+  );
+};
+
+export default NavBar;
